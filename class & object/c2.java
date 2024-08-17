d19d78c1afec1929cd14001694bb8dbfb9b22908
iAmrudra-nil <cse22007@cemk.ac.in>
2024-08-17 08:34:43 +0530

    adding new class

---
+++ class & object/c2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+//importing util java library class to execute Scanner class 
+class car{
+    //car class have 3 properties of price,name,milage
+    float price;
+    String name;
+    int milage;
+}
+class c2 {
+    public static void main(String[] args) {
+        car c1=new car();//making an object of car class
+
+        Scanner sc=new Scanner(System.in);//scanner class object to take input from user
+
+        System.out.print("Enter car price:");
+        float price=sc.nextFloat();
+        System.out.print("Enter car name:");
+        String name=sc.next();
+        System.out.print("Enter car milage:");
+        int milage=sc.nextInt();
+
+        //assigning car object value
+        c1.price=price;
+        c1.milage=milage;
+        c1.name=name;
+        //showing result what it gives
+        System.out.println("Price:"+c1.price+" "+"Name"+ c1.name+" "+"Mileage:"+c1.milage);
+    }
+}
