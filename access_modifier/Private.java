6e20c6b88b77b0153c5406ee5ee74d466a9f41b4
iAmrudra-nil <cse22007@cemk.ac.in>
2024-09-01 09:28:38 +0530

    adding new file

--- access_modifier/Private.java
+++ access_modifier/Private.java
@@ -1,6 +1,6 @@
-/*We can use private access modifier to method and attributes or variables. private access
+/*We can use private access modifier in methods and variables but not in classes. private access
  * modifier has a scope in the same class that means in same class we can access any private methods 
- * or private attributes out of that class it has no scpoe
+ * or private attributes out of that class it has no scpoe.
  */
  class Private {
     private  int a=67;
@@ -8,8 +8,8 @@
         System.out.println("This is private method..");
     }
     public static void main(String[] args) {
-        System.out.println(a);//another hacks is that a static method cannot 
-        display();
+        System.out.println(a);//another hacks is that non-static variable or methods cannot be referenced from a static context
+        display();//therefore as main class is by convension is static so if we have to reffer any method or variable we have to convert them into satic
     }
     
 }
---
+++ access_modifier/default.java
@@ -0,0 +1,19 @@
+/*default access modifier  scope is in the same package.We can create default classes, objects and variables.
+we can type default keyword or write no keyword to show default access modifier
+ */
+class abc{
+    private int n=90;
+     int a=67;
+}
+class d extends abc{
+    int b=9;
+}
+class def{
+    public static void main(String[] args) {
+        d ob1=new d();
+        System.out.println(ob1.a);
+        System.out.println(ob1.b);
+        //System.out.println(ob1.n);it gives error as private variables does not inherit
+
+    }
+}
